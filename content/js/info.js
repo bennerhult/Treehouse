c58a44ac83b58bd4288bb7f8f0d885a40ee60728
Linda Karlsson <linda@lejbrinkbennerhult.se>
2012-05-11 14:15:40 +0200

    margin on text content

--- content/js/info.js
+++ content/js/info.js
@@ -12,6 +12,7 @@ function showInfo(content) {
 
 function getAchievementInfo() {
     return (
+    '<div>' +
     '<h2>What is an Achievement?</h2>' +
     '<p><b>achievement</b> [uh-cheev-muhnt] <br>  something accomplished, especially by superior ability, special effort, great courage, etc.; a great or heroic deed.</p>' +
     '<h2>Why should I want to achieve something?</h2>' +
@@ -23,14 +24,17 @@ function getAchievementInfo() {
     '<h2>Is it free?</h2>' +
     '<p>It is free.</p>' +
     '<h2>Can my friends see my achievements?</h2>' +
-    '<p>When you want to share your feeling of accomplishment (the technical term would be brag) you can publicize your achievements. You can easily share to Facebook or copy the link and share it however you want. We\'ve heard that some people still use email. If you do not share an achievement, only you can see it. Nothing wrong with tracking your success and be proud all by yourself.</p>'
+    '<p>When you want to share your feeling of accomplishment (the technical term would be brag) you can publicize your achievements. You can easily share to Facebook or copy the link and share it however you want. We\'ve heard that some people still use email. If you do not share an achievement, only you can see it. Nothing wrong with tracking your success and be proud all by yourself.</p>' +
+    '</div>'
         )
 }
 
 function getEarlyAdopterInfo() {
     return (
-        '<h2>the Coveted Early Adopter Achievement</h2>' +
-            '<p>You are so much of an early adopter that you cannot even get it yet! Impressive! Sign up now and you will be first in line when it appears!</p>'
+        '<div>' +
+            '<h2>the Coveted Early Adopter Achievement</h2>' +
+            '<p>You are so much of an early adopter that you cannot even get it yet! Impressive! Sign up now and you will be first in line when it appears!</p>' +
+        '</div>'
         )
 }
 
