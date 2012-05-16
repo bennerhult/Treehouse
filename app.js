66950555442773b1844a8fcad2fb72e300af16bf
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2012-05-16 16:19:58 +0200

    cannot progress in public view

--- app.js
+++ app.js
@@ -286,7 +286,7 @@ function getGoalText(goal, achievement, progressNumber, progressPercentage, publ
                                         + goal.quantityTotal
                                         + '</h3>'
                                     + '</td>';
-                                if (progressPercentage < 100) {
+                                if (!publicView && progressPercentage < 100) {
                                     goalText    += '<td><div id="addbutton' + goal._id + '" class="addbutton">'
                                         + '<a href="javascript:void(0)" onclick="progress(\'' + goal._id + '\', \'' +  goal.quantityTotal + '\')">'
                                         + '<img src="content/img/+.png" alt="I did it!"/>'
