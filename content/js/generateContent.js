23fc733e9591aaf422872d134c2bfd21c2c08694
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2012-08-27 16:11:54 +0200

    bug fix

--- content/js/generateContent.js
+++ content/js/generateContent.js
@@ -46,24 +46,23 @@ function setCreateEditMenu(data) {
 }
 
 function setDefaultMenu(bothCompletedAndNotExists) {
-    var menu = '<div id="menu"><ul><li id="inProgress">'
+    var menu = '<div id="menu"><ul><li  id="inProgress">'
     if (bothCompletedAndNotExists) {
-        menu +=  '<a href="javascript:void(0)" onclick="getAchievements(false)"><span>in progress</span></a>'
+        menu +=  '<a href="javascript:void(0)" onclick="getAchievements(false)"><span id="inProgressSpan" class="hoverDesktop">in progress</span></a>'
     }
     menu += '</li><li id="menuToggle"><a href="javascript:void(0)" onclick="toggleTab()"><img src="content/img/tree-tab.png" alt=""/></a></li><li id="completed">'
     if (bothCompletedAndNotExists) {
-        menu +=  '<a href="javascript:void(0)" onclick="getAchievements(true)"><span>completed</span></a>'
+        menu +=  '<a href="javascript:void(0)" onclick="getAchievements(true)"><span id="completedSpan" class="hoverDesktop">completed</span></a>'
     }
     menu +=  '</li></ul></div>' + getTabMenu(bothCompletedAndNotExists)
     $("#menuArea").html(menu)
 }
 
 function removeHoverState(){
-   // $("body").removeClass("menu ul li span:hover");
-
-    $('#inProgress').hover(function() {
-        $("#inProgress").removeClass()
-    });
+    $("#inProgressSpan").removeClass()
+    $("#inProgressSpan").attr("class","iDevice")
+    $("#completedSpan").removeClass()
+    $("#completedSpan").attr("class","iDevice")
 }
 
 function setAchievementMenu(publiclyVisible, progressMade, isLatestAchievement, completed, userId) {
