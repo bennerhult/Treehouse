88567b34c2c9f1ae08ca9397d8d3b3ae7d422748
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2012-08-29 20:50:50 +0200

    fb login for web app

--- content/js/treehouseFunctions.js
+++ content/js/treehouseFunctions.js
@@ -32,7 +32,7 @@ function loginUsingFacebook() {
                         if (data == "ok") { //TODO: use ajax success/error instead
                             openAchievements(false)
                         } else {
-                            $("#message").html('Facebook did not play nice: ' + data)
+                            $("#message").html('Facebook did not play nice. Sorry, ' + me.email)
           				}
 					}
 				)
