2ff7eee52005f27ae11595f69897b611f2d5fb68
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2012-09-03 13:04:19 +0200

    onOrientationChange: redraw

--- content/js/generateContent.js
+++ content/js/generateContent.js
@@ -33,14 +33,15 @@ function init() {
     jQuery(window).bind('orientationchange', function() {
         switch ( window.orientation ) {
             case 0: //Portrait orientation. This is the default value.
-
                 $("#app-container").scrollTop(0)
+                $("#achievementList").scrollTop(0)
+                alert( $("#achievementList").scrollTop())
                 $("#app-container").hide()
                 $("#app-container").show()
                 break
             case 180: //Portrait orientation with the screen turned upside down. This value is currently not supported on iPhone.
-
                 $("#app-container").scrollTop(0)
+                $("#achievementList").scrollTop(0)
                 $("#app-container").hide()
                 $("#app-container").show()
                 break
