384d871cc1b55208514af97aa7de6652c7b16c7f
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2012-09-27 17:07:06 +0200

    add friend

--- app.js
+++ app.js
@@ -338,7 +338,6 @@ function getSignupErrorMessage (err){
 app.get('/findFriends', function(request, response){
     user.User.findOne({ username: request.query.friend_email.toLowerCase() }, function(err,foundFriend) {
         if (foundFriend)    {
-
             if (request.session.user_id == foundFriend._id ) {
                 response.send('Dissociative identity disorder?', { 'Content-Type': 'application/json' }, 404)
             }  else {
