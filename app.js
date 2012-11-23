9057ee0f91a046b187d1cf96f55480a05bdc051c
MLejbrink <mattias@lejbrinkbennerhult.se>
2012-11-23 13:33:15 +0100

    Tweet achievement.

--- app.js
+++ app.js
@@ -633,7 +633,7 @@ function writeAchievementPage(response, currentViewedAchieverId, currentAchievem
                                 achievementDesc += '<div id="fbLike" style="overflow:visible;"><div class="fb-like" data-send="false" data-width="250" data-show-faces="true" font="segoe ui"></div></div>'
                                 achievementDesc += '<br />'
                                 achievementDesc += '<div id="tweetAchievement" style="overflow:visible;">'
-                                achievementDesc += '<a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>'
+                                achievementDesc += '<a href="https://twitter.com/share' + '?text=' + currentAchievement.title + '" class="twitter-share-button">Tweet</a>'
                                 achievementDesc += '<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>'
                                 achievementDesc += '</div>'
                                 achievementDesc += '<br />'
