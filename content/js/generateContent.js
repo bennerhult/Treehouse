5b0d1d81f4275c2c923d89d12c1b9f11359852b4
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2013-08-14 10:30:27 +0200

    refactoring: removed redundant css class and conditional

--- content/js/generateContent.js
+++ content/js/generateContent.js
@@ -398,21 +398,9 @@ function getCookie(c_name) {
 
 function getAchievementsContent(achieverId, lookingAtFriend, callback) {
     getPrettyNameFromServer(achieverId, function(prettyName) {
-        var content =  '<div id="contentwrap"> <div id="userarea"><img src="content/img/user_has_no_image.jpg" /><p>' + prettyName + '</p></div> <div id="achievementListTabs"><a href="javascript:void(0)" onclick="getAchievements(false, \'' + achieverId + '\', ' + lookingAtFriend + ')"><span id="inProgressSpan" class="'
-            if  (isiPad || isiPhone) {   //TODO Remove this conditional and css classes, they are identical?
-                content+= 'iDevice'
-            } else {
-                content+= 'hoverDesktop'
-            }
-        content +=  '">Challenges</span></a>'
+        var content =  '<div id="contentwrap"> <div id="userarea"><img src="content/img/user_has_no_image.jpg" /><p>' + prettyName + '</p></div> <div id="achievementListTabs"><a href="javascript:void(0)" onclick="getAchievements(false, \'' + achieverId + '\', ' + lookingAtFriend + ')"><span id="inProgressSpan" class="hoverDesktop">Challenges</span></a>'
 
-        content +=  '<a href="javascript:void(0)" onclick="getAchievements(true, \'' + achieverId + '\', ' + lookingAtFriend + ')"><span id="completedSpan" class="'
-            if  (isiPad || isiPhone) {    //TODO Remove this conditional and css classes, they are identical?
-                content+= 'iDevice'
-            } else {
-                content+= 'hoverDesktop'
-            }
-        content +=  '">Unlocked</span></a></div>'
+        content +=  '<a href="javascript:void(0)" onclick="getAchievements(true, \'' + achieverId + '\', ' + lookingAtFriend + ')"><span id="completedSpan" class="hoverDesktop">Unlocked</span></a></div>'
 
         content +=  '<div id="achievementList"></div></div>'
         callback(content)
