2133fb2322e9b26d5934d842877f513a142979e3
unknown <EBennerhult@W8WM31.avida.local>
2014-01-20 09:21:35 +0100

    improved test suite

--- test/test.js
+++ test/test.js
@@ -11,7 +11,11 @@ describe('Treehouse existence', function() {
         })
     })
 
-    //TODO: need to set up test data to make this work on everybody's computer
+    //TODO: create test user
+    //TODO: login
+    //TODO: create achievement
+    //TODO: todo publicize
+    //TODO: use achievement and user created to test public achievement below
     it('public achievement works', function(done) {
         request.get('http://localhost:1337/achievement?achievementId=524087e03d90f8181200008d&userId=50c5f49c9400f66c170000fd').end(function(res) {
             expect(res).to.exist
@@ -21,14 +25,14 @@ describe('Treehouse existence', function() {
         })
     })
 
-
-    //TODO: this should not work when not logged in!
-    it('private achievement works', function(done) {
-        request.get('http://localhost:1337/achievement?achievementId=52160a55d2eda1441d0002c7&userId=50b4ecda20d743b019000031').end(function(res) {
+    //TODO: use achievement and user created to test private achievement below
+    it('private achievement shown when logged in', function(done) {
+        request.get('http://localhost:1337/achievement?achievementId=52160a55d2eda1441d0002c7&auserId=50b4ecda20d743b019000031').end(function(res) {
             expect(res).to.exist
             expect(res.status).to.equal(200)
-            expect(res.text).to.contain('page')
+            expect(res.text).to.contain('många')
             done()
         })
     })
+
 })
