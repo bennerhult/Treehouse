039fcbc32b69162d60877c488b0ef48173a10543
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2014-02-04 22:14:03 +0100

    converting app-container facebook like to fb-feed for improved customization

--- app.js
+++ app.js
@@ -345,7 +345,6 @@ function getDataForUser(myUser, request, response, newUser, appMode) {
             })
         }
     } else {    //Sign up
-        console.log("SIGN UP: " + appMode)
         user.createUser(email, function (newUser,err) {
             if (err) {
                 response.writeHead(200, {'content-type': 'application/json' })
@@ -1296,7 +1295,11 @@ function writeAchievementPage(response, achiever, currentAchievement, userId, is
                                                     var encodedAchLink = encodeURIComponent(achLink)
                                                     achievementDesc += '<div id="sharer-container"></div><div id="compare-container"></div>'
                                                    // achievementDesc += '<div class="publicWrap"><div id="fbLike" style="overflow:visible;"><iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Ftreehouse.io&amp;width=292&amp;layout=button_count&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=21&amp;appId=480961688595420" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:21px;" allowTransparency="true"></iframe></div>'
-                                                    achievementDesc += '<div class="publicWrap"><div id="fbLike"><a onclick="fbShare(\''+ currentAchievement.title + '\', \'' +  achLink +'\', \'' + currentAchievement.imageURL + '\')" href="javascript:void(0)">like</a></div></div>'
+                                                    achievementDesc += '<div class="publicWrap"><div id="fbLike" style="overflow:visible;">'
+                                                    achievementDesc += '<a href="http://www.facebook.com/plugins/like.php?=' + encodedAchLink + '&width&layout=standard&action=like&show_faces=true&share=false&height=80&appId=480961688595420">Like </a>'
+                                                    //achievementDesc += '<div class="fb-like" data-href="http://treehouse.io/achievement?achievementId=' + currentAchievement._id + '&userId=' + currentAchievement.createdBy + '" data-width="300px" font="segoe ui" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>'
+                                                    achievementDesc += '<a onclick="fbShare(\''+ currentAchievement.title + '\', \'' +  achLink +'\', \'' + currentAchievement.imageURL + '\')" href="javascript:void(0)">share to fb</a>'
+                                                    achievementDesc += '</div></div>'
                                                     achievementDesc += '<div id="tweetAchievement" style="overflow:visible;">'
                                                     achievementDesc += '<a href="https://twitter.com/share?url=' + encodedAchLink + '&text=' + currentAchievement.title + '" class="twitter-share-button" data-via="Treehouse">Tweet</a>'
                                                     achievementDesc +='<script type="text/javascript">(function() {var s = document.createElement("SCRIPT");var c = document.getElementsByTagName("script")[0];s.type = "text/javascript";s.async = true;s.src = "http://platform.twitter.com/widgets.js";c.parentNode.insertBefore(s, c);})();</script>'
