61353a3997499aaeca0946ff492bf586fcd5a0c7
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2014-02-05 14:25:51 +0100

    fixing app-container fb-like layout

--- app.js
+++ app.js
@@ -1294,15 +1294,14 @@ function writeAchievementPage(response, achiever, currentAchievement, userId, is
                                                     //using public domain - localhost makes FB-like component not load since localhost is not registered as a Treehouse app domain
                                                     var achLink = "http://treehouse.io/achievement?achievementId=" + currentAchievement._id + "&userId=" + currentAchievement.createdBy
 
-
                                                     var encodedAchLink = encodeURIComponent(achLink)
                                                     achievementDesc += '<div id="sharer-container"></div><div id="compare-container"></div>'
-                                                    achievementDesc += '<div class="publicWrap"><div id="fbLike" style="overflow:visible;">'
+                                                    achievementDesc += '<div class="publicWrap"><div id="fbLike" style="overflow: visible">'
 
-                                                    achievementDesc +='<iframe src="//www.facebook.com/plugins/like.php?href=' + encodedAchLink + '&amp;width&amp;layout=button_count&amp;locale=en_US&amp;action=like&amp;show_faces=true&amp;share=false&amp;height=80&amp;appId=480961688595420" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:80px;" allowTransparency="true"></iframe>'
+                                                    achievementDesc +='<iframe width="95" src="//www.facebook.com/plugins/like.php?href=' + encodedAchLink + '&amp;width&amp;layout=button_count&amp;locale=en_US&amp;action=like&amp;show_faces=true&amp;share=false&amp;height=80&amp;appId=480961688595420" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:80px;" allowTransparency="true"></iframe>'
 
-                                                    achievementDesc += '<a onclick="fbShare(\''+ currentAchievement.title + '\', \'' +  achLink +'\', \'' + currentAchievement.imageURL + '\')" href="javascript:void(0)">share</a>'
-                                                    achievementDesc += '</div></div>'
+                                                    achievementDesc += '</div><div id="fbShare"><a onclick="fbShare(\''+ currentAchievement.title + '\', \'' +  achLink +'\', \'' + currentAchievement.imageURL + '\')" href="javascript:void(0)">share</a>'
+                                                    achievementDesc += '</div>'
                                                     achievementDesc += '<div id="tweetAchievement" style="overflow:visible;">'
                                                     achievementDesc += '<a href="https://twitter.com/share?url=' + encodedAchLink + '&text=' + currentAchievement.title + '" class="twitter-share-button" data-via="Treehouse">Tweet</a>'
                                                     achievementDesc +='<script type="text/javascript">(function() {var s = document.createElement("SCRIPT");var c = document.getElementsByTagName("script")[0];s.type = "text/javascript";s.async = true;s.src = "http://platform.twitter.com/widgets.js";c.parentNode.insertBefore(s, c);})();</script>'
