544993ee1e80b8bf27bde67c4326b8f67a303942
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2014-02-10 15:44:52 +0100

    fixing bug where add-to-home-ballon could not be closed

--- content/js/add2home.js
+++ content/js/add2home.js
@@ -19,7 +19,7 @@ var addToHome = (function (w) {
 
         options = {
             autostart: true,			// Automatically open the balloon
-            returningVisitor: false,	 // Show the balloon to returning visitors only (setting this to true is highly recommended)
+            returningVisitor: true,	 // Show the balloon to returning visitors only (setting this to true is highly recommended)
             animationIn: 'drop',		// drop || bubble || fade
             animationOut: 'fade',		// drop || bubble || fade
             startDelay: 2000,			// 2 seconds from page load before the balloon appears
