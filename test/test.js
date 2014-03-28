e26dd86bb536c2195ceb39ed42992a9938c00c9d
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2014-03-28 13:24:00 +0100

    refactoring

--- test/test.js
+++ test/test.js
@@ -10,7 +10,7 @@ casper.test.begin('Testing Start Page', 3, function(test) {
     casper.thenOpen('http://localhost:1337', function() {})
 
     casper.then(function(){
-        test.assertHttpStatus(200);
+        test.assertHttpStatus(200)
         test.assertTitle('Treehouse', 'Start page achievement has correct title')
         test.assertTextExists('connect below', 'page body contains dom only text "connect below "')
     })
@@ -24,8 +24,8 @@ casper.test.begin('Testing Start Page', 3, function(test) {
      casper.start('http://localhost:1337/checkUser?username=tester@treehouse.io&appMode=false')
 
      casper.then(function(){
-         test.assertHttpStatus(201);     //new user
-         loginToken = JSON.parse(this.getPageContent());
+         test.assertHttpStatus(201)    //new user
+         loginToken = JSON.parse(this.getPageContent())
      })
 
      casper.run(function(){
@@ -76,7 +76,7 @@ casper.test.begin('Testing create token', 1, function(test){
      casper.start('http://localhost:1337/checkUser?username=tester@treehouse.io&appMode=false')
 
      casper.then(function(){
-         test.assertHttpStatus(200);     //existing user
+         test.assertHttpStatus(200)    //existing user
          loginToken = JSON.parse(this.getPageContent())
      })
 
@@ -89,7 +89,7 @@ casper.test.begin('Testing fetching userId', 1, function(test){
     casper.start('http://localhost:1337/userIdForUsername?username=tester@treehouse.io')
 
     casper.then(function() {
-        test.assertHttpStatus(200);
+        test.assertHttpStatus(200)
         userId1 = JSON.parse(this.getPageContent())
     })
 
@@ -102,7 +102,7 @@ casper.test.begin('Testing setting pretty name', 1, function(test){
     casper.start('http://localhost:1337/setPrettyName?user_id=' + userId1 + '&firstName=Tester&lastName=Schmester')
 
     casper.then(function() {
-        test.assertHttpStatus(200);
+        test.assertHttpStatus(200)
     })
 
     casper.run(function() {
@@ -114,7 +114,7 @@ casper.test.begin('Testing correct Sign in', 3, function(test){
     casper.start('http://localhost:1337/signin?email=tester@treehouse.io&token=' + loginToken + '&appMode=false')
 
     casper.then(function() {
-        test.assertHttpStatus(200);
+        test.assertHttpStatus(200)
         test.assertTitle('Treehouse - Newsfeed', 'Sign on show correct title')
         test.assertTextExists('Tester Schmester', 'page contains "Tester Schmester"')
     })
@@ -133,7 +133,7 @@ casper.test.begin('Testing Create Achievement', 1, function(test){
     casper.start('http://localhost:1337/newAchievement?user_id=' + userId1 + '&title=TestAchievement&description=Testing achievement&currentImage=treehouse.io/content/img/achievementImages/1.png&goalTitles=["testGoal1"]&goalQuantities=2')
 
     casper.then(function(){
-        test.assertHttpStatus(200);
+        test.assertHttpStatus(200)
         achievementId1 = JSON.parse(this.getPageContent())
     })
 
@@ -147,7 +147,7 @@ casper.test.begin('Testing Private Achievement when signed in', 3, function(test
     casper.start('http://localhost:1337/achievement?achievementId=' + achievementId1 + '&userId=' + userId1)
 
     casper.then(function(){
-        test.assertHttpStatus(200);
+        test.assertHttpStatus(200)
         test.assertTitle('Treehouse - TestAchievement', 'Public achievement has correct title')
         test.assertTextExists('Tester Schmester', 'Public achievement has correct user')
     })
@@ -161,7 +161,7 @@ casper.test.begin('Testing deleting Achievement when signed in', 2, function(tes
     casper.start('http://localhost:1337/delete?achievementId=' + achievementId1)
 
     casper.then(function(){
-        test.assertHttpStatus(200);
+        test.assertHttpStatus(200)
         test.assertTextExists('ok', 'Delete reported ok')
     })
 
@@ -175,7 +175,7 @@ casper.test.begin('Testing Public Achievement', 3, function(test){
     casper.start('http://localhost:1337/achievement?achievementId=52dcfd394e67c8880c000002&userId=50b4ecda20d743b019000031')
 
     casper.then(function(){
-     test.assertHttpStatus(200);
+     test.assertHttpStatus(200)
      test.assertTitle('Treehouse - test4', 'Private achievement accessible when signed in')
      test.assertTextExists('by Tester Schmester', 'Public achievement has correct user')
      test.assertTextExists('test4', 'page body contains "test4"')
@@ -190,7 +190,7 @@ casper.test.begin('Testing sign out', 3, function(test){
     casper.start('http://localhost:1337/signout')
 
     casper.then(function(){
-        test.assertHttpStatus(200);
+        test.assertHttpStatus(200)
         test.assertTitle('Treehouse', 'Correct title')
         test.assertTextExists('connect below', 'page body contains "connect below"')
     })
@@ -204,7 +204,7 @@ casper.test.begin('Testing reusing token ', 3, function(test){
     casper.start('http://localhost:1337/signin?email=tester@treehouse.io&token=' + loginToken + '&appMode=false')
 
     casper.then(function() {
-        test.assertHttpStatus(200);
+        test.assertHttpStatus(200)
         test.assertTitle('Treehouse', 'Should lead to start page')
         test.assertTextExists('connect below', 'page body contains "connect below"')
     })
@@ -218,7 +218,7 @@ casper.test.begin('Testing reusing token ', 3, function(test){
      casper.start('http://localhost:1337/achievement?achievementId=52dcfd394e67c8880c000002&userId=50b4ecda20d743b019000031')
 
      casper.then(function(){
-         test.assertHttpStatus(200);
+         test.assertHttpStatus(200)
          test.assertTitle('Treehouse', 'Private achievement not accessible when not signed in')
          test.assertTextExists('connect below ', 'page body contains dom only text "connect below "')
      })
