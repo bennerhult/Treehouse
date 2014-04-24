612ba863a811efdb667f589ba565edff29579445
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2014-04-24 14:27:06 +0200

    removed app mode email login, does not work with iOS7

--- code/requestHandlers.js
+++ code/requestHandlers.js
@@ -43,7 +43,7 @@ function topIndexPart(userId, nrOfFriendShipRequests) {
         tab + '<script src="content/ext/js/facebook.js" type="text/javascript"></script>' + nl +
         tab + '<script src="content/ext/js/jquery-1.10.2.js" type="text/javascript"></script>' + nl +
         tab + '<script type="application/javascript" src="content/js/add2home.js"></script>' + nl +
-        tab + '<script type="application/javascript" src="content/js/generateContent.js?" + thVersion + "></script>' + nl +
+        tab + '<script type="application/javascript" src="content/js/generateContent.js?' + thVersion + '></script>' + nl +
         tab + '<script type="application/javascript" src="content/js/info.js"></script>' + nl +
         tab + '<script type="application/javascript" src="content/js/treehouseFunctions.js"></script>' + nl +
         tab + '<script type="application/javascript" src="content/js/magnetic.js"></script>' + nl +
