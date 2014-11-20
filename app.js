69a439ef53178a0a0d46345a3329e2441b30019a
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2014-11-20 22:21:34 +0100

    upgraded to express 4.x

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var db_uri = 'mongodb://localhost:27017/test';
 var domain = '';
 var app = express();
 //app.use(bodyParser());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json({ extended: false }));
 //app.use(express.json());
 //app.use(express.urlencoded());
 var MongoStore = require('connect-mongo')(session);
