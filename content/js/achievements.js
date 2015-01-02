5dea32f5b5b2f98dc68a8901148eb4a535f1be13
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2015-01-02 18:32:41 +0100

    opening correct achievement

--- content/js/achievements.js
+++ content/js/achievements.js
@@ -1,12 +1,25 @@
 var achievementApp = angular.module('App', ['ngRoute']);
 
+achievementApp.factory("achievementService",function(){
+    var service = {};
+    service.achievement = 0;
+
+    service.setAchievement = function(currentAchievement){
+        this.achievement = currentAchievement;
+    }
+
+    return service;
+});
+
 achievementApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider
-        //TODO visa achievement snyggt
-        //TODO fixa pretty URLS https://scotch.io/quick-tips/pretty-urls-in-angularjs-removing-the-hashtag
-        //TODO kunna refresha ett achievement
-        //TODO flytta alla sidor till singlePageApp
-        .when('/achievement/:achievementId', {
+        //TODO 6 kunna växla genom att klicka flera på raken eller dölja listan emellan?
+        //TODO 5 visa achievement snyggt
+        //TODO 4 kunna refresha ett achievement
+        //TODO 3 dela upp achievment/achievmentList till två moduler
+        //TODO 2 fixa pretty URLS https://scotch.io/quick-tips/pretty-urls-in-angularjs-removing-the-hashtag
+        //TODO 1 flytta alla sidor till singlePageApp
+        .when('/achievement', {
             templateUrl: '/server-templates/achievement.html',
             controller: 'achievementController'
         })
@@ -15,16 +28,24 @@ achievementApp.config(['$routeProvider', function($routeProvider) {
         });*/
 }]);
 
-achievementApp.controller('Ctrl', function($scope, $http) {
+achievementApp.controller('Ctrl', function($scope, $http, achievementService) {
     $scope.isLoading = true;
     $http.post('/api/achievements/init', {}).success(function(result) {
         $scope.achievementList = result.achievementList;
+        achievementService.setAchievement($scope.achievementList[1]);
         $scope.prettyName = result.prettyName;
         $scope.userImageURL = result.userImageURL;
         $scope.isLoading = false;
     });
+
+    $scope.openAchievement = function (chosenAchievement) {
+        console.log("opening achievement: " + chosenAchievement )
+        achievementService.setAchievement(chosenAchievement);
+    };
 });
 
-achievementApp.controller('achievementController', function($scope,  $routeParams) {
+achievementApp.controller('achievementController', function($scope,  $routeParams, achievementService) {
     $scope.achievementId = $routeParams.achievementId;
+    $scope.achievement = achievementService.achievement;
+
 });
