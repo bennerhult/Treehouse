95c3d370dd033d0e950108fdd860d4376d45575d
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2015-01-09 16:03:10 +0100

    stub for upload user image

--- content/js/more.js
+++ content/js/more.js
@@ -2,6 +2,11 @@ treehouseApp.controller('moreController', function($scope, $http, pageService) {
     "use strict";
     pageService.setTitle('More');
 
+    $scope.uploadUserImage = function(evt, newsItem) {
+        evt.preventDefault();
+        console.log("uploading user IMAGE"); //TODO upload user image
+    }
+
     $scope.signout = function(evt) {
         evt.preventDefault();
 
