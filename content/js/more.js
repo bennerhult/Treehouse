a84891261e091084a4ed9877d55f49d6a0e5180b
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2015-01-19 10:41:44 +0100

    debugging

--- content/js/more.js
+++ content/js/more.js
@@ -15,6 +15,7 @@ treehouseApp.controller('moreController', function($scope, $http, pageService) {
         filepicker.pick({container: container,
             services: ['COMPUTER', 'FACEBOOK', 'IMAGE_SEARCH', 'URL', 'INSTAGRAM', 'FLICKR', 'DROPBOX', 'PICASA', 'GOOGLE_DRIVE', 'SKYDRIVE','WEBDAV', 'EVERNOTE', 'GMAIL', 'GITHUB']}, function(inkBlob){
 
+            alert(pageService.isiOs)
             if (pageService.isiOs) {
                 $("#imageUploadFrame").remove();
             }
