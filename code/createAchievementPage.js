dfe8c8fd372c5b01cafbe3899600d6aecc903aea
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2015-02-02 12:46:57 +0100

    create achievement now with th-images

--- code/createAchievementPage.js
+++ code/createAchievementPage.js
@@ -10,7 +10,7 @@ module.exports = function (app, templates, requestHandlers, user, progress, mome
         app.post('/api/achievements/createAchievement', function (request, response) {
            // var goals = [{title: 'testTitle', quantity: 1}, {title: 'testTitle2', quantity: 5}];
             var goals = [{title: 'testTitle', quantity: 1}];
-            achievement.createAchievement2(request.session.currentUser._id, request.body.achievementTitle, request.body.achievementDescription, "../content/img/achievementImages/48.png", goals, function() {
+            achievement.createAchievement2(request.session.currentUser._id, request.body.achievementTitle, request.body.achievementDescription, request.body.achievementImage, goals, function() {
                 return requestHandlers.respondWithJson(response, {url: thSettings.getDomain() + 'app/achievements'});
             });
         });
@@ -19,7 +19,4 @@ module.exports = function (app, templates, requestHandlers, user, progress, mome
     return {
         registerHandlers : registerHandlers
     };
-
-
-
 }
--- content/js/createAchievement.js
+++ content/js/createAchievement.js
@@ -1,11 +1,77 @@
 treehouseApp.controller('createAchievementController', function($scope,  $http, pageService) {
     pageService.setTitle('Create new achievement');
 
+    var images = [
+        "/content/img/achievementImages/1.png",
+        "/content/img/achievementImages/2.png",
+        "/content/img/achievementImages/3.png",
+        "/content/img/achievementImages/4.png",
+        "/content/img/achievementImages/5.png",
+        "/content/img/achievementImages/6.png",
+        "/content/img/achievementImages/7.png",
+        "/content/img/achievementImages/8.png",
+        "/content/img/achievementImages/9.png",
+        "/content/img/achievementImages/10.png",
+        "/content/img/achievementImages/11.png",
+        "/content/img/achievementImages/12.png",
+        "/content/img/achievementImages/13.png",
+        "/content/img/achievementImages/14.png",
+        "/content/img/achievementImages/15.png",
+        "/content/img/achievementImages/16.png",
+        "/content/img/achievementImages/17.png",
+        "/content/img/achievementImages/18.png",
+        "/content/img/achievementImages/19.png",
+        "/content/img/achievementImages/20.png",
+        "/content/img/achievementImages/21.png",
+        "/content/img/achievementImages/22.png",
+        "/content/img/achievementImages/23.png",
+        "/content/img/achievementImages/24.png",
+        "/content/img/achievementImages/25.png",
+        "/content/img/achievementImages/26.png",
+        "/content/img/achievementImages/27.png",
+        "/content/img/achievementImages/28.png",
+        "/content/img/achievementImages/29.png",
+        "/content/img/achievementImages/30.png",
+        "/content/img/achievementImages/31.png",
+        "/content/img/achievementImages/32.png",
+        "/content/img/achievementImages/33.png",
+        "/content/img/achievementImages/34.png",
+        "/content/img/achievementImages/35.png",
+        "/content/img/achievementImages/36.png",
+        "/content/img/achievementImages/37.png",
+        "/content/img/achievementImages/38.png",
+        "/content/img/achievementImages/39.png",
+        "/content/img/achievementImages/40.png",
+        "/content/img/achievementImages/41.png",
+        "/content/img/achievementImages/42.png",
+        "/content/img/achievementImages/43.png",
+        "/content/img/achievementImages/44.png",
+        "/content/img/achievementImages/45.png",
+        "/content/img/achievementImages/46.png",
+        "/content/img/achievementImages/47.png",
+        "/content/img/achievementImages/48.png",
+        "/content/img/achievementImages/49.png"
+    ];
+
     $scope.createAchievement = function(evt) {
         evt.preventDefault();
         $http.post('/api/achievements/createAchievement', {
             achievementTitle : $scope.achievementTitle,
-            achievementDescription: $scope.achievementDescription
+            achievementDescription : $scope.achievementDescription,
+            achievementImage : $("#achievementImage").attr("src")
         }).success(function(result) {});
+    };
+
+    $scope.toggleImage = function(evt, step) {
+        evt.preventDefault();
+        var currentImage = $("#achievementImage").attr("src");
+        var currentPos = jQuery.inArray(currentImage, images);
+        var newPos = currentPos + step;
+        if (newPos  >= images.length) {
+            newPos = 0;
+        }   else if (newPos == -1) {
+            newPos = images.length-1;
+        }
+        $("#achievementImage").attr("src", images[newPos]);
     };
 });
