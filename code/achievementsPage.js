4ad88002a6ecc59604f43fb1bdb1a885599e6dbf
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2015-02-02 13:28:15 +0100

    removed unused code

--- code/achievementsPage.js
+++ code/achievementsPage.js
@@ -12,35 +12,4 @@ module.exports = function (app, templates, requestHandlers, user, progress, mome
     return {
         registerHandlers : registerHandlers
     };
-
-
-    function showAchievementPage(request, response) {
-        var url_parts = url.parse(request.url, true)
-        var currentAchievementId = url_parts.query.achievementId.trim()
-        var isNotificationViewString = url_parts.query.isNotificationView.trim()
-        var isNotificationView = (isNotificationViewString === 'true')
-        var sharerId
-        if (isNotificationView) {
-            sharerId = url_parts.query.sharerId.trim()
-        }
-        var achieverId = url_parts.query.achieverId
-        progress.Progress.findOne({ achievement_id: currentAchievementId, achiever_id: achieverId }, function(err,currentProgress) {
-            request.session.current_achievement_id = currentAchievementId
-            achievement.Achievement.findOne({ _id: currentAchievementId }, function(err,currentAchievement) {
-                user.User.findOne({ _id: achieverId }, function(err,currentAchiever) {
-                    if (request.session.currentUser) {
-                         writeAchievementPage(response, currentAchiever, currentAchievement, request.session.currentUser._id, isNotificationView, sharerId);
-                    } else if (currentAchievement && (currentProgress.publiclyVisible || currentAchievement.issuedAchievement))    {
-                        writeAchievementPage(response, currentAchiever, currentAchievement, null, isNotificationView, sharerId);
-                    } else {
-                        requestHandlers.writeDefaultPage(request, response);
-                    }
-                })
-            })
-        })
-    }
-
-    return {
-        showAchievementPage : showAchievementPage
-    };
 }
