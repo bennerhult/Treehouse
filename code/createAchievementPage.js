39cd79d1d0e1f243efe1e83aeb786a140226b4f9
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2015-02-02 12:49:48 +0100

    removed todos

--- code/createAchievementPage.js
+++ code/createAchievementPage.js
@@ -2,11 +2,6 @@ module.exports = function (app, templates, requestHandlers, user, progress, mome
     'use strict';
 
     function registerHandlers() {
-        //TODO ett achievement ska bara dyka upp en gång, oavsett antalet progresses
-        //TODO öppna achievements efter sparad
-        //TODO verifiera inmatningsdata
-        //TODO spara bara progresses (i achievement.js) om achievementet sparas
-
         app.post('/api/achievements/createAchievement', function (request, response) {
            // var goals = [{title: 'testTitle', quantity: 1}, {title: 'testTitle2', quantity: 5}];
             var goals = [{title: 'testTitle', quantity: 1}];
