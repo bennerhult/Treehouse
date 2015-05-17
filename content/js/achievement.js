e95c8de278cb03b842f834da86bd7140bf0e3cd0
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2015-05-17 20:57:21 +0200

    fixed back so that delete works

--- content/js/achievement.js
+++ content/js/achievement.js
@@ -58,7 +58,7 @@ treehouseApp.controller('achievementController', function($scope, $http, achieve
 
     $scope.deleteAchievement = function(evt) {
         $http.post('/api/achievements/deleteAchievement', {
-            achievement : $scope.achievement
+            achievementInstance : $scope.achievement
         }).success(function() {});
     };
 });
