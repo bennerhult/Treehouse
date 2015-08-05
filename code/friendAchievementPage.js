77f691aa757db0d3c411d376aac4269958137b47
Erik Bennerhult <erik@lejbrinkbennerhult.se>
2015-08-05 22:08:15 +0200

    added a convenience method isVisibleToUser, to determine whether a user has the right to see an achievmentInstance

--- code/friendAchievementPage.js
+++ code/friendAchievementPage.js
@@ -4,40 +4,44 @@ module.exports = function (app, friendship, user, requestHandlers, achievementIn
     var _ = require("underscore")._
 
     app.post('/api/friendAchievement/compareList', function (request, response) {
-            var userId = request.body.friend._id;
-            var achievementId = request.body.achievementInstance.achievementId
-            achievementInstance.getCompareList(userId, achievementId, function(compareList) {
-                var result = new Object();
-                result.compareList = compareList;
-                requestHandlers.respondWithJson(response, result);
-            });
+        var userId = request.body.friend._id;
+        var achievementId = request.body.achievementInstance.achievementId
+        achievementInstance.getCompareList(userId, achievementId, function (compareList) {
+            var result = new Object();
+            result.compareList = compareList;
+            requestHandlers.respondWithJson(response, result);
         });
-        
+    });
+
     function registerHandlers() {
         app.post('/api/friendAchievement/init', function (request, response) {
             var friendUserId = request.body.friendUserId;
             var friendAchievementId = request.body.achievementId;
-            user.User.findOne({ _id: friendUserId }, function(err, friendUser) {
-                if(err) {
+            user.User.findOne({ _id: friendUserId }, function (err, friendUser) {
+                if (err) {
                     throw err;
                 }
-                if(!friendUser) {
-                    throw 'no such user'
+                if (!friendUser) {
+                    throw 'no such user';
                 }
-                
-                achievementInstance.AchievementInstance.findOne({ _id : friendAchievementId }, function(err2, ai) {
-                    if(ai.publiclyVisible !== true) {
-                        throw 'haxorattempt!'
-                    }
-                    var u = { }
+                achievementInstance.AchievementInstance.findOne({ _id: friendAchievementId }, function (err2, ai) {
+                    achievementInstance.AchievementInstance.findOne({
+                        achievementId: ai.achievementId,
+                        createdBy: friendUserId
+                    }, function (err, eventualSharedAchievementInstance) {
+                        if (!achievementInstance.isVisibleToUser(ai, request.session.currentUser._id, eventualSharedAchievementInstance)) {
+                            throw 'haxorattempt!';
+                        }
+                    });
 
+                    var u = {}
                     u.friend = {
-                        _id : friendUser._id,
-                        username : friendUser.username,
-                        prettyName : user.getPrettyName(friendUser),
-                        imageURL : friendUser.imageURL
+                        _id: friendUser._id,
+                        username: friendUser.username,
+                        prettyName: user.getPrettyName(friendUser),
+                        imageURL: friendUser.imageURL
                     }
-                    
+
                     u.achievement = ai
                     requestHandlers.respondWithJson(response, u)
                 })
@@ -46,6 +50,6 @@ module.exports = function (app, friendship, user, requestHandlers, achievementIn
     }
 
     return {
-        registerHandlers : registerHandlers
+        registerHandlers: registerHandlers
     };
 };
--- models/achievementInstance.js
+++ models/achievementInstance.js
@@ -4,40 +4,41 @@ var mongoose = require('mongoose'),
     user = require('./user.js'),
     achievement = require('./achievement.js'),
     newsfeedEvent = require('./newsfeedEvent.js'),
-    Schema= mongoose.Schema;
+    Schema = mongoose.Schema;
 
 var schemaOptions = {
     toObject: {
         virtuals: true,
         methods: true
     }
-    ,toJSON: {
+    , toJSON: {
         virtuals: true,
         methods: true
     }
 };
 
 var AchievementInstanceSchema = new Schema({
-    createdDate             : {type: Date, required: true},
-    unlockedDate            : {type: Date},
-    createdBy               : {type: Schema.ObjectId, required: true},
-    originalCreatedBy       : {type: Schema.ObjectId, required: true},
-    createdByName           : {type: String, required: true},
-    createdByImageURL       : {type: String, required: true},
-    achievementId           : {type: Schema.ObjectId, required: true},
-    title                   : {type: String, required: true},
-    description             : {type: String},
-    imageURL                : {type: String, required: true},
-    publiclyVisible         : {type: Boolean, required: true},
-    isShared                : {type: Boolean, required: true},
-    percentageCompleted     : {type: Number, required: true},
-    goals                   : {type: [goal.GoalSchema], required: true}
+    createdDate: { type: Date, required: true },
+    unlockedDate: { type: Date },
+    createdBy: { type: Schema.ObjectId, required: true },
+    originalCreatedBy: { type: Schema.ObjectId, required: true },
+    createdByName: { type: String, required: true },
+    createdByImageURL: { type: String, required: true },
+    achievementId: { type: Schema.ObjectId, required: true },
+    title: { type: String, required: true },
+    description: { type: String },
+    imageURL: { type: String, required: true },
+    publiclyVisible: { type: Boolean, required: true },
+    isShared: { type: Boolean, required: true },
+    percentageCompleted: { type: Number, required: true },
+    goals: { type: [goal.GoalSchema], required: true }
 }, schemaOptions);
 
 var AchievementInstance = mongoose.model('AchievementInstance', AchievementInstanceSchema);
 
 module.exports = {
     AchievementInstance: AchievementInstance,
+    isVisibleToUser: isVisibleToUser,
     createAchievement: createAchievement,
     createIssuedAchievement: createIssuedAchievement,
     acceptIssuedAchievement: acceptIssuedAchievement,
@@ -61,12 +62,12 @@ function createAchievement(createdBy, title, description, imageURL, goals, callb
     myAchievement.title = title;
     myAchievement.description = description;
     myAchievement.imageURL = imageURL;
-    async.each(goals, function(currentGoal, goalProcessed ) {
+    async.each(goals, function (currentGoal, goalProcessed) {
         var myGoal = goal.prepareGoal(currentGoal.title, currentGoal.quantity);
         myAchievement.goals.push(myGoal);
         goalProcessed();
-    }, function() {
-        myAchievement.save(function(error) {
+    }, function () {
+        myAchievement.save(function (error) {
             createAchievementInstance(myAchievement, createdBy, callback)
         });
     });
@@ -87,7 +88,7 @@ function createAchievementInstance(motherAchievement, user, callback, more) {
     myAchievementInstance.isShared = false;
     myAchievementInstance.percentageCompleted = 0;
     myAchievementInstance.goals = motherAchievement.goals;
-    if(more) {
+    if (more) {
         myAchievementInstance.publiclyVisible = more.publiclyVisible;
     }
     myAchievementInstance.save(function (error) {
@@ -95,28 +96,56 @@ function createAchievementInstance(motherAchievement, user, callback, more) {
     });
 }
 
-AchievementInstanceSchema.virtual('createdByUser').get(function() {
+AchievementInstanceSchema.virtual('createdByUser').get(function () {
     if (this.createdBy.equals(this.originalCreatedBy)) {
         return true;
-    } else  {
+    } else {
         return false;
     }
 });
 
+/*
+before calling isVisibleToUser, if you have a possible shared achievement, check to see if there exists 
+a shared achievement by checking if there is an achievementInstance (ai) with the same 
+(parent)achievementId and created by the user in question, as in:
+                achievementInstance.AchievementInstance.findOne({
+                        achievementId: ai.achievementId,
+                        createdBy: friendUserId
+                    },function (err, eventualSharedAchievementInstance) {
+                        if (achievementInstance.isVisibleToUser(ai, request.session.currentUser._id, eventualSharedAchievementInstance)) {                    
+*/
+function isVisibleToUser(achievementInstance, userId, eventualSharedAchievementInstance) {
+    if (achievementInstance.createdBy.equals(userId)) {
+        return true;
+    } else if (achievementInstance.originalCreatedBy.equals(userId)) {
+        return true;
+    } else if (achievementInstance.publiclyVisible) {
+        return true;
+    } else if (achievementInstance.isShared) {
+        if (eventualSharedAchievementInstance) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
 function createIssuedAchievement(createdBy, title, description, imageURL, issuerName) {
     var achievementInstance = new AchievementInstance();
-   /* achievement.createdDate = new Date();
-    achievement.createdBy = createdBy;
-    achievement.title = title;
-    achievement.description = description;
-    achievement.imageURL = imageURL;
-    achievement.issuedAchievement = true;
-    achievement.issuerName = issuerName;*/
+    /* achievement.createdDate = new Date();
+     achievement.createdBy = createdBy;
+     achievement.title = title;
+     achievement.description = description;
+     achievement.imageURL = imageURL;
+     achievement.issuedAchievement = true;
+     achievement.issuerName = issuerName;*/
     return achievementInstance;
 }
 
 function getPublicAchievement(achievementInstanceId, callback) {
-    AchievementInstance.findById(achievementInstanceId, function(err1, currentAchievementInstance) {
+    AchievementInstance.findById(achievementInstanceId, function (err1, currentAchievementInstance) {
         if (currentAchievementInstance && currentAchievementInstance.publiclyVisible && currentAchievementInstance.publiclyVisible === true) {
             callback(currentAchievementInstance);
         } else {
@@ -126,34 +155,34 @@ function getPublicAchievement(achievementInstanceId, callback) {
 }
 
 function getAchievementList(userId, callback) {
-    AchievementInstance.find({ 
-            createdBy: userId, 
-            unlockedDate: { $exists: false } 
-        }, {}, { sort: { 'created' : -1 } }, function(err, achievementInstancesInProgress) {
-        AchievementInstance.find({ createdBy: userId, unlockedDate: { $exists: true }}, {}, { sort: { 'created' : -1 } }, function(err2, achievementInstancesUnlocked) {
+    AchievementInstance.find({
+        createdBy: userId,
+        unlockedDate: { $exists: false }
+    }, {}, { sort: { 'created': -1 } }, function (err, achievementInstancesInProgress) {
+        AchievementInstance.find({ createdBy: userId, unlockedDate: { $exists: true } }, {}, { sort: { 'created': -1 } }, function (err2, achievementInstancesUnlocked) {
             callback(achievementInstancesInProgress, achievementInstancesUnlocked);
         });
     });
 }
 
 function getCompareList(userId, achievementId, callback) {
-    AchievementInstance.find({ 
-            achievementId: achievementId,
-            createdBy: {'$ne': userId}, 
-        }, {}, { sort: { 'created' : -1 } }, 
-        function(err, compareList) {
+    AchievementInstance.find({
+        achievementId: achievementId,
+        createdBy: { '$ne': userId },
+    }, {}, { sort: { 'created': -1 } },
+        function (err, compareList) {
             callback(compareList);
-    });
+        });
 }
 
 function userHasAcceptedAchievement(achievement_id, achiever_id, callback) {
-   /* progress.Progress.findOne({ achievement_id: achievement_id, achiever_id: achiever_id}, function(err,currentAchievement) {
-        if (currentAchievement) {
-            callback(true);
-        } else {
-            callback(false);
-        }
-    });*/
+    /* progress.Progress.findOne({ achievement_id: achievement_id, achiever_id: achiever_id}, function(err,currentAchievement) {
+         if (currentAchievement) {
+             callback(true);
+         } else {
+             callback(false);
+         }
+     });*/
 }
 
 function acceptIssuedAchievement(achievement_id, achiever_id, callback) {
@@ -168,10 +197,10 @@ function acceptIssuedAchievement(achievement_id, achiever_id, callback) {
 }
 
 function issue(achievement, callback) {
-   /* achievement.isIssued = true;
-    achievement.save(function (error) {
-        callback(error);
-    });*/
+    /* achievement.isIssued = true;
+     achievement.save(function (error) {
+         callback(error);
+     });*/
 }
 
 function progress(goalToUpdate, achievementToUpdate, callback) {
@@ -179,11 +208,11 @@ function progress(goalToUpdate, achievementToUpdate, callback) {
 }
 
 function progressByCount(goalToUpdate, achievementToUpdate, quantityToProgress, callback) {
-    if  (goalToUpdate.quantityCompleted < goalToUpdate.quantityTotal) {
+    if (goalToUpdate.quantityCompleted < goalToUpdate.quantityTotal) {
         var newQuantityCompleted = 0;
         var newQuantityTotal = 0;
         var newGoalCompleted;
-        async.each(achievementToUpdate.goals, function(currentGoal, goalProcessed) {
+        async.each(achievementToUpdate.goals, function (currentGoal, goalProcessed) {
             if (currentGoal._id == goalToUpdate._id) {
                 currentGoal.quantityCompleted = currentGoal.quantityCompleted + quantityToProgress;
                 newGoalCompleted = currentGoal.quantityCompleted;
@@ -191,19 +220,19 @@ function progressByCount(goalToUpdate, achievementToUpdate, quantityToProgress, 
             newQuantityCompleted += currentGoal.quantityCompleted;
             newQuantityTotal += currentGoal.quantityTotal;
             goalProcessed();
-        }, function(){
-            achievementToUpdate.percentageCompleted = 100 * (newQuantityCompleted/newQuantityTotal);
-           if (newQuantityCompleted >= newQuantityTotal) {
+        }, function () {
+            achievementToUpdate.percentageCompleted = 100 * (newQuantityCompleted / newQuantityTotal);
+            if (newQuantityCompleted >= newQuantityTotal) {
                 achievementToUpdate.unlockedDate = new Date();
             }
             var id = achievementToUpdate._id;
             delete achievementToUpdate._id;
-            if(achievementToUpdate.toObject) {
+            if (achievementToUpdate.toObject) {
                 //The findAndUpdate code explodes if achievementToUpdate is a mongoose object so this wierdness is needed
                 achievementToUpdate = achievementToUpdate.toObject()
             }
             AchievementInstance.findByIdAndUpdate(id, achievementToUpdate, function (err, updatedAchievement) {
-                 callback(updatedAchievement);
+                callback(updatedAchievement);
             });
         });
     } else {
@@ -230,10 +259,10 @@ function unpublicize(achievementInstance, callback) {
 }
 
 function remove(achievementInstance, next) {
-    AchievementInstance.findById(achievementInstance._id, function(err1, currentAchievementInstance) {
+    AchievementInstance.findById(achievementInstance._id, function (err1, currentAchievementInstance) {
         var achievementId = achievementInstance.achievementId;
         currentAchievementInstance.remove(function () {
-            achievement.Achievement.findById( achievementId, function (err2, currentAchievement) {
+            achievement.Achievement.findById(achievementId, function (err2, currentAchievement) {
                 currentAchievement.remove(function () {
                     if (next) {
                         next();
